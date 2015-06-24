4fa48fecec10410b6f14850018ce9dffaf462c59
David van Leusen <jvleusen@gmail.com>
2015-06-25 00:28:13 +0200

    Add more safety-checks to the assembler, add documentation and switch the default ExecutorService to a single asynchronous thread.

--- src/main/java/nl/rug/search/cpptool/api/io/Assembler.java
+++ src/main/java/nl/rug/search/cpptool/api/io/Assembler.java
@@ -2,7 +2,6 @@ package nl.rug.search.cpptool.api.io;
 
 import com.google.common.base.Throwables;
 import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.MoreExecutors;
 import nl.rug.search.cpptool.api.DeclContainer;
 import nl.rug.search.cpptool.runtime.RuntimeAssembler;
 
@@ -10,7 +9,15 @@ import javax.annotation.Nonnull;
 import java.io.File;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+/**
+ * The assembler takes a set of CppTool .pb input files and asynchronously reads and merges the data they contain into
+ * a single declaration tree.
+ *
+ * @author David van Leusen <J.D.van.leusen@student.rug.nl>
+ * @since 2015-06-24
+ */
 public interface Assembler {
     @Nonnull
     static Assembler create(final @Nonnull ExecutorService executor) {
@@ -19,12 +26,27 @@ public interface Assembler {
 
     @Nonnull
     static Assembler create() {
-        return create(MoreExecutors.newDirectExecutorService());
+        return create(Executors.newSingleThreadExecutor());
     }
 
+    /**
+     * Adds the given {@link File} to the input for the declaration tree builder.
+     * <br />
+     * Duplicate input (same file path) will be ignored.
+     *
+     * @param input CppTool .pb data file
+     * @return this object for method chaining
+     * @throws IllegalStateException if called after calling {@link #deferBuild()}
+     */
     @Nonnull
     Assembler read(final @Nonnull File input);
 
+    /**
+     * Signal the assembler to finalize the input and begin building the declaration tree.
+     * Once this method has been called, it is no longer possible to add input to the assembler.
+     *
+     * @return A future of the declaration container.
+     */
     @Nonnull
     ListenableFuture<DeclContainer> deferBuild();
 
--- src/main/java/nl/rug/search/cpptool/runtime/RuntimeAssembler.java
+++ src/main/java/nl/rug/search/cpptool/runtime/RuntimeAssembler.java
@@ -2,6 +2,7 @@ package nl.rug.search.cpptool.runtime;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.*;
 import nl.rug.search.cpptool.api.DeclContainer;
 import nl.rug.search.cpptool.api.io.Assembler;
@@ -9,6 +10,7 @@ import nl.rug.search.cpptool.api.io.Assembler;
 import javax.annotation.Nonnull;
 import java.io.File;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -19,6 +21,7 @@ public class RuntimeAssembler implements Assembler {
     private final AtomicBoolean built = new AtomicBoolean(false);
     private final ListeningExecutorService executor;
     private final List<ListenableFuture<PartialResult>> results = Lists.newLinkedList();
+    private final Set<String> addedFiles = Sets.newHashSet();
     private ListenableFuture<DeclContainer> finalResult = null;
 
     public RuntimeAssembler(final @Nonnull ExecutorService executor) {
@@ -29,7 +32,14 @@ public class RuntimeAssembler implements Assembler {
     @Override
     public Assembler read(@Nonnull File input) {
         checkState(!built.get(), "Assembler.deferBuild has already been invoked");
-        results.add(executor.submit(new PartialResultBuilder(checkNotNull(input, "input == NULL"))));
+        checkNotNull(input, "input == NULL");
+
+        final String filePath = input.getAbsolutePath();
+        if (!addedFiles.contains(filePath)) {
+            results.add(executor.submit(new PartialResultBuilder(input)));
+            addedFiles.add(filePath);
+        }
+        
         return this;
     }
 
@@ -37,9 +47,9 @@ public class RuntimeAssembler implements Assembler {
     @Override
     public synchronized ListenableFuture<DeclContainer> deferBuild() {
         if (built.get()) {
+            checkState(this.finalResult != null, "Assembler.deferBuild called but result is missing.");
             return this.finalResult;
         } else {
-            built.set(true);
             this.finalResult = Futures.transform(
                     Futures.allAsList(this.results),
                     this::mergeResults,
@@ -62,6 +72,7 @@ public class RuntimeAssembler implements Assembler {
                     cleanup();
                 }
             }, executor);
+            built.set(true);
             return this.finalResult;
         }
     }
