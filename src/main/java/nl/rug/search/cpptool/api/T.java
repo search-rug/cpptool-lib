7fc88851101a93fe2d6aad06a9d2cd5c8797bfc3
David van Leusen <jvleusen@gmail.com>
2015-06-25 00:28:39 +0200

    Add more utility predicates to T

--- src/main/java/nl/rug/search/cpptool/api/T.java
+++ src/main/java/nl/rug/search/cpptool/api/T.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Iterables;
 import nl.rug.search.cpptool.api.data.CxxFunction;
 import nl.rug.search.cpptool.api.data.CxxRecord;
 import nl.rug.search.cpptool.api.data.CxxVariable;
+import nl.rug.search.cpptool.api.data.Record;
 
 import java.util.Set;
 
@@ -16,11 +17,23 @@ public class T {
         throw new ExceptionInInitializerError(T.class.getCanonicalName() + " cannot be instantiated.");
     }
 
-    public static boolean isClass(Declaration d) {
+    public static boolean isCxxClass(Declaration d) {
         return d.declarationType() == DeclType.RECORD && d.has(CxxRecord.class);
     }
 
     public static boolean isClassMember(Declaration d) {
         return Iterables.any(CXX_MEMBERS, d::has);
+    }
+
+    public static boolean isFunction(Declaration d) {
+        return d.declarationType() == DeclType.FUNCTION;
+    }
+
+    public static boolean isVariable(Declaration d) {
+        return d.declarationType() == DeclType.VARIABLE;
+    }
+
+    public static boolean isCRecord(Declaration d) {
+        return d.declarationType() == DeclType.RECORD && d.has(Record.class) && !d.has(CxxRecord.class);
     }
 }
